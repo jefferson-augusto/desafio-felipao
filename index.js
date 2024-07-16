9bbb1faeff684e9eb2253d8988c7ab9ac4a25cfb
JEFFERSON OLIVEIRA <jefguto@yahoo.com>
2024-07-16 19:19:08 -0300

    meu primeiro

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ let xp = 8500
 if (xp <= 1000)
         console.log("O Heroi de nome," +nheroi + "esta no nivel " +Ferro)
   else;
-    if (xp >=1001 || xp <= 2000)
+    if (xp >=1001 && xp <= 2000)
          console.log("O Heroi de nome," +nheroi + "esta no nivel " +Bronze)
       else
-        if (xp >=2001 || xp <= 5000)
+        if (xp >=2001 && xp <= 5000)
          console.log("O Heroi de nome," +nheroi + "esta no nivel " +Prata)
           else
-             if (xp >=5001 || xp <=6000)
+             if (xp >=5001 && xp <=6000)
                console.log("O Heroi de nome," +nheroi + "esta no nivel " +Ouro) 
                else
-                 if (xp >= 6001 || xp <=7001)
+                 if (xp >= 6001 && xp <=7001)
                    console.log("O Heroi de nome," +nheroi + "esta no nivel " +Platina)
                      else
-                       if (xp >=7001 || xp <= 8000)
+                       if (xp >=7001 && xp <= 8000)
                          console.log("O Heroi de nome," +nheroi + "esta no nivel " +Diamante)
                            else
-                          if (xp >=8001 || xp <= 9000)
+                          if (xp >=8001 && xp <= 9000)
                              console.log("O Heroi de nome," +nheroi + "esta no nivel " +Acendente)
                                else
-                                 if (xp >=9001 || xp <= 10000)
+                                 if (xp >=9001 && xp <= 10000)
                                    console.log("O Heroi de nome," +nheroi + "esta no nivel " +Imortal)
                                    else
                                        console.log("O Heroi de nome," +nheroi + "esta no nivel " +Radiante)
